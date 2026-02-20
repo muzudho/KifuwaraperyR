57487f42337d46e87da9bd0a4e01b94306760e65
muzudho <muzudho1@gmail.com>
2026-02-21 03:31:35 +0900

    include

--- src/lib_toybox_n2/char_to_piece_usi.hpp
+++ src/lib_toybox_n2/char_to_piece_usi.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../lib_toybox_n1/piece.hpp"
+#include "../lib_toybox_n1/muz_position_n1_model.hpp"
 #include <map>
 
 /// <summary>
--- src/lib_toybox_n2/squares.hpp
+++ src/lib_toybox_n2/squares.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "../lib_toybox_n1/file.hpp"
-#include "../lib_toybox_n1/rank.hpp"
-#include "../lib_toybox_n1/square.hpp"
+#include "../lib_toybox_n1/muz_position_n1_model.hpp"
 
 /// <summary>
 /// 
