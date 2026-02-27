d1ab56518977b32bb0ccf1711e9249df1238b904
muzudho <muzudho1@gmail.com>
2026-02-27 20:56:37 +0900

    フォルダー構成変更

--- src/main.hpp
+++ src/main.hpp
@@ -4,7 +4,7 @@
 
 #pragma once
 #include "game_engine/muz_game_engine_service.hpp"
-#include "application/muz_logging_service.hpp"
+#include "n91a_application/muz_logging_service.hpp"
 #include <iostream>
 #include <cstdio>    // printf を使うために必要
 #include <filesystem>
