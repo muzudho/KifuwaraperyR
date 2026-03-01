c2f021ec3dccf7224c92d5d88e4cd2d4a7a2b188
muzudho <muzudho1@gmail.com>
2026-03-01 22:54:54 +0900

    WIP: Usi move

---
+++ src/n23a_usi_string_and_object/usi_string_and_move.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/// <summary>
+///     <pre>
+/// TODO: USIの文字列と、指し手オブジェクトを相互変換するクラスだぜ（＾～＾）
+///     </pre>
+/// </summary>
+class UsiStringAndMove
+{
+public:
+
+    void convert_usi_to_body(const std::string& usi_str);
+};
