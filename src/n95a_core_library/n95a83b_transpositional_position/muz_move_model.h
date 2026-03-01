57c6041556c461bb755eb64ae5ba05fb25191fbe
muzudho <muzudho1@gmail.com>
2026-03-01 20:31:50 +0900

    move

--- src/n95a_core_library/n95a83b_transpositional_position/muz_move_model.h
+++ src/n95a_core_library/n95a83b_transpositional_position/muz_move_model.h
@@ -13,12 +13,17 @@
 /// </summary>
 class MuzMoveModel
 {
-public:
+
+
+    // ========================================
+    // フィールド
+    // ========================================
 
 
     // [移動元]
 
 
+private:
     /// <summary>
     ///     <pre>
     /// 移動元のマス。
@@ -117,5 +122,109 @@ public:
     // TODO: ［移動元］と［移動先］（＾～＾）
     // 
     // TODO: ［移動元］と［移動先］と［成り］（＾～＾）
+
+
+    // ========================================
+    // アクセッサー
+    // ========================================
+
+
+public:
+
+
+    /// <summary>
+    ///     <pre>
+    /// 移動元のマス。
+    /// 
+    ///     - 旧名： `Move.from()`
+    ///     </pre>
+    /// </summary>
+    Square get_source_sq() const { return this->source_sq_; }
+
+
+    /// <summary>
+    ///     <pre>
+    /// 移動する前の駒の種類（盤上にある駒の場合）。
+    /// 
+    ///     - 旧名： `Move.pieceTypeFrom()`
+    ///     </pre>
+    /// </summary>
+    PieceType get_source_pt() const { return this->source_pt_; }
+
+
+    /// <summary>
+    ///     <pre>
+    /// 移動する前の駒の種類（打つ場合）。
+    /// 
+    ///     - 旧名： `Move.pieceTypeDropped()`
+    ///     </pre>
+    /// </summary>
+    PieceType get_source_dropped_pt() const { return this->source_dropped_pt_; }
+
+
+    // ［移動による変化］
+
+
+    /// <summary>
+    ///     <pre>
+    /// 成るか。
+    /// 
+    ///     - 旧名： `Move.isPromotion()`
+    ///     </pre>
+    /// </summary>
+    bool is_promotion() const { return this->is_promotion_; }
+
+
+    /// <summary>
+    ///     <pre>
+    /// 打か。
+    /// 
+    ///     - 旧名： `Move.isDrop()`
+    ///     </pre>
+    /// </summary>
+    bool is_drop() const { return this->is_drop_; }
+
+
+    /// <summary>
+    ///     <pre>
+    /// 駒を取ったか。
+    /// 
+    ///     - 旧名： `Move.isCapture()`
+    ///     </pre>
+    /// </summary>
+    bool is_capture() const { return this->is_capture_; }
+
+
+    /// <summary>
+    ///     <pre>
+    /// 駒を取った場合の、取った駒の種類。
+    /// 
+    ///     - 旧名： `Move.cap()`
+    ///     </pre>
+    /// </summary>
+    PieceType capture_pt() const { return this->capture_pt_; }
+
+
+    // ［移動先］
+
+
+    /// <summary>
+    ///     <pre>
+    /// 移動先のマス。
+    /// 
+    ///     - 旧名： `Move.to()`
+    ///     </pre>
+    /// </summary>
+    Square destination_sq() const { return this->destination_sq_; }
+
+
+    /// <summary>
+    ///     <pre>
+    /// 移動した後の駒の種類。
+    /// 
+    ///     - 旧名： `Move.pieceTypeTo()`
+    ///     </pre>
+    /// </summary>
+    PieceType destination_pt() const { return this->destination_pt_; }
 };
 
