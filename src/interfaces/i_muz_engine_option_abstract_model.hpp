5a4eb093a3d94f10e3ca4b61f78f948e93da23f6
muzudho <muzudho1@gmail.com>
2026-03-07 17:14:36 +0900

    wip: インターフェース

---
+++ src/interfaces/i_muz_engine_option_abstract_model.hpp
@@ -0,0 +1,79 @@
+﻿#pragma once
+#include <functional>
+#include <string>
+#include <format>  // C++20
+
+/// <summary>
+/// エンジン・オプション的☆（＾ｑ＾）
+/// </summary>
+class IMuzEngineOptionAbstractModel
+{
+
+
+public:
+
+
+	// ========================================
+	// 演算子
+	// ========================================
+
+
+	/// <summary>
+    /// 代入演算子☆
+	/// </summary>
+	/// <param name="v"></param>
+	/// <returns></returns>
+	IMuzEngineOptionAbstractModel& operator = (const std::string& v);
+
+
+	/// <summary>
+    /// 整数化☆
+	/// </summary>
+	virtual operator int() const = 0;
+
+
+	/// <summary>
+    /// 文字列化☆
+	/// </summary>
+	virtual operator std::string() const = 0;
+
+
+	// ========================================
+	// その他のメソッド
+	// ========================================
+
+
+	/// <summary>
+	/// 既定値
+	/// </summary>
+	/// <returns></returns>
+	virtual std::string	GetDefaultValue() const = 0;
+
+
+	/// <summary>
+	/// 型
+	/// </summary>
+	/// <returns></returns>
+	virtual std::string	GetType() const = 0;
+
+
+	/// <summary>
+	/// 最小値
+	/// </summary>
+	/// <returns></returns>
+	virtual int GetMin() const = 0;
+
+
+	/// <summary>
+	/// 最大値
+	/// </summary>
+	/// <returns></returns>
+	virtual int GetMax() const = 0;
+
+
+	/// <summary>
+    /// USIコード化☆（＾～＾）
+	/// </summary>
+	/// <returns></returns>
+	virtual std::string ToUSICode()	const = 0;
+};
